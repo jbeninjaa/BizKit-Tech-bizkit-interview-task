0db267fd6c6140286e6cd06201def75b2d6abc1d
Jayven Lupera <jveeen.lupera@gmail.com>
2024-10-23 23:34:16 +0800

    Update match.py
    Improved the matching algorithm's time complexity from O(n x m) to O(nlogn + mlogm),  where n and m are size of both the list respectively.

--- phasebook/match.py
+++ phasebook/match.py
@@ -20,8 +20,34 @@ def match(match_id):
 
 
 def is_match(fave_numbers_1, fave_numbers_2):
+    # == Prev code
+    '''
     for number in fave_numbers_2:
         if number not in fave_numbers_1:
             return False
 
     return True
+    '''
+
+    #  == New code
+
+    # Sort both both list
+    fave_numbers_1.sort()
+    fave_numbers_2.sort()
+
+    # Initialize Variables
+    pointer_fn1 = 0 
+    pointer_fn2 = 0
+    len_fn1 = len(fave_numbers_1) 
+    len_fn2 = len(fave_numbers_2)
+
+    # Traverse 
+    while(pointer_fn1 < len_fn1 and pointer_fn2 < len_fn2):
+        # If we find a match, we increase both pointers to check the next index
+        if(fave_numbers_1[pointer_fn1] == fave_numbers_2[pointer_fn2]):
+            pointer_fn2 += 1
+        pointer_fn1 += 1 # If we don't find a match, increase pointer for fn1 to look for a match.
+
+    # if pointer_fn2 reaches the length of fn2, it means all numbers in fn2 is present in fn1
+    return pointer_fn2 == len_fn2
+    
