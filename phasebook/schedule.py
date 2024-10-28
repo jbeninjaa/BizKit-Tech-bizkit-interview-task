524a3b894fb4b34cd74727dff3be019c2fcec4a1
Jayven Lupera <jveeen.lupera@gmail.com>
2024-10-28 13:06:51 +0800

    documentation for search_data.py

--- phasebook/schedule.py
+++ phasebook/schedule.py
@@ -13,23 +13,29 @@ def hello():
  
 @bp.route("<int:user_id>", methods=["POST"])
 def schedule(user_id):
+    # parse request body
     schedule_json = request.json
 
+    # extract request data
     start_sched = schedule_json[0]["start"]
     end_sched = schedule_json[0]["end"]
+
 
     schedules = []
     schedule = start_sched + " - " + end_sched
 
+    # check if user already exists 
     user_exists = any(user["user_id"] == str(user_id) for user in USER_SCHEDULES)
 
     if user_exists:
+        # find the user_id and append the schedule if not yet present
         for user in USER_SCHEDULES:
             if user["user_id"] == str(user_id) and schedule not in user["schedule"]:
                 user["schedule"].append(schedule)
             return USER_SCHEDULES
         
     else:
+        # create a schedule for the user
         schedules.append(schedule)
         user_schedule = {
             "user_id": str(user_id),
---
+++ search_data.py
@@ -0,0 +1,44 @@
+USERS = [
+    {
+        "age": 29,
+        "id": "1",
+        "name": "John Doe",
+        "occupation": "Developer"
+    },
+    {
+        "age": 30,
+        "id": "2",
+        "name": "Jane Doe",
+        "occupation": "Engineer"
+    },
+    {
+        "age": 25,
+        "id": "3",
+        "name": "Joe Doe",
+        "occupation": "Designer"
+    },
+    {
+        "age": 28,
+        "id": "4",
+        "name": "John Smith",
+        "occupation": "Architect"
+    },
+    {
+        "age": 31,
+        "id": "5",
+        "name": "Jane Smith",
+        "occupation": "Manager"
+    },
+    {
+        "age": 24,
+        "id": "6",
+        "name": "Joe Smith",
+        "occupation": "Designer"
+    },
+    {
+        "age": 29,
+        "id": "7",
+        "name": "John Doe",
+        "occupation": "Developer"
+    }
+]
